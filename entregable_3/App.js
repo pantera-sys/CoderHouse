10f337dce8b831cb97c475c1cf8195819d809313
pantera-sys <josenaranjo2004.jn@gmail.com>
2022-06-07 21:50:13 -0500

    entregable #3

---
+++ entregable_3/App.js
@@ -0,0 +1,65 @@
+class store {
+    constructor( products, address, propietary) {
+        
+        this.products = products;
+        this.address = address;
+        this.propietary = propietary;
+    }
+}
+
+let tiendas = {}
+
+const $ = document
+
+function createShop() {
+    const nameStore = $.getElementById('shop').value
+    const address = $.getElementById('address').value
+    const propietary = $.getElementById('propietary').value
+    const products = $.getElementById('product').value
+
+    if (!nameStore ) {
+        console.log('y el nombre perro?')
+    }
+    else if (!address) {
+        console.log('y la direccion?')
+    }
+    else if (!propietary) {
+        console.log('y el propietario?')
+    }
+    else if (!products) {
+        console.log('y los productos?')
+    }
+    else {
+        const nameStores = Object.keys(tiendas)
+        if (!nameStores.includes(nameStore)) {
+            console.log('exito perro')
+            tiendas[nameStore] = new store(products, address, propietary)
+        }else {
+            console.log('ya existe')
+        }
+    }   
+}
+
+
+
+function showInformation() {
+    const informacion = $.getElementById('info')
+    const datos = []
+    tiendas.map(function(tienda) {
+        tienda.map(function(info) {
+            datos.push(`<h3>${info.nameStore}</h3>
+            <p>${info.address}</p>
+            <p>${info.propietary}</p>
+            <p>${info.products}</p>`)
+        }
+    )
+    }
+    )
+    informacion.innerHTML = [...datos]
+}
+
+const btnShop = $.getElementById('btnShop')
+const btnShow = $.getElementById('btnShow')
+
+btnShow.addEventListener('click', showInformation)
+btnShop.addEventListener('click', createShop)
