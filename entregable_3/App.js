3a1634945a50dbf23628a179331c4b49ca28e8f4
pantera-sys <josenaranjo2004.jn@gmail.com>
2022-06-09 17:36:02 -0500

    testing improvements

--- entregable_3/App.js
+++ entregable_3/App.js
@@ -7,7 +7,7 @@ class store {
     }
 }
 
-let tiendas = {}
+
 
 const $ = document
 
@@ -17,6 +17,7 @@ function createShop() {
     const propietary = $.getElementById('propietary').value
     const products = $.getElementById('product').value
 
+    let tiendas = []
     if (!nameStore ) {
         console.log('y el nombre?')
     }
@@ -30,11 +31,13 @@ function createShop() {
         console.log('y los productos?')
     }
     else {
-        const nameStores = Object.keys(tiendas)
-        if (!nameStores.includes(nameStore)) {
-            console.log('exito ')
-            tiendas[nameStore] = new store(products, address, propietary)
-        }else {
+        const names = tiendas?.map(tiendas => console.log(tiendas))
+        console.log(names)
+        if (nameStore.includes(names)) {
+            const shop = new store(products, address, propietary)
+            tiendas.push(shop)
+            console.log(tiendas)
+        }else{
             console.log('ya existe')
         }
     }   
@@ -59,7 +62,16 @@ function createShop() {
 // }
 
 const btnShop = $.getElementById('btnShop')
-const btnShow = $.getElementById('btnShow')
+// const btnShow = $.getElementById('btnShow')
 
-btnShow.addEventListener('click', showInformation)
+// btnShow.addEventListener('click', showInformation)
 btnShop.addEventListener('click', createShop)
+
+
+// const nameStores = Object.keys(tiendas)
+        // if (!nameStores.includes(nameStore)) {
+        //     console.log('exito ')
+        //     tiendas[nameStore] = new store(products, address, propietary)
+        // }else {
+        //     console.log('ya existe')
+        // }
